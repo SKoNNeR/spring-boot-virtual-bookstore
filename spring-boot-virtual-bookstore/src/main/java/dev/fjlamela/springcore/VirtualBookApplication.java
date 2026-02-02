0d45ca15de48064921b61123d55816fe2600b4fa
Francisco José Lamela López <skonner87@gmail.com>
2026-02-02 10:06:11 +0100

    run Spring Boot and expose books endpoints

--- spring-boot-virtual-bookstore/src/main/java/dev/fjlamela/springcore/VirtualBookApplication.java
+++ spring-boot-virtual-bookstore/src/main/java/dev/fjlamela/springcore/VirtualBookApplication.java
@@ -1,9 +1,14 @@
 package dev.fjlamela.springcore;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class VirtualBookApplication {
+	public static void main(String[] args) {
 
+		SpringApplication.run(VirtualBookApplication.class, args);
+		
+	}
 }
 	
