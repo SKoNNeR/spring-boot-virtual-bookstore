bb9ab61e16f8df31bfb48a8803eecbcefff587de
Francisco José Lamela López <skonner87@gmail.com>
2026-02-02 09:11:15 +0100

    initial pass to spring boot proyect

---
+++ spring-boot-virtual-bookstore/src/dev/fjlamela/springcore/repository/inmemory/InMemoryBookRepository.java
@@ -0,0 +1,45 @@
+package dev.fjlamela.springcore.repository.inmemory;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import org.springframework.stereotype.Repository;
+
+import dev.fjlamela.springcore.domain.Book;
+import dev.fjlamela.springcore.repository.BookRepository;
+
+@Repository
+public class InMemoryBookRepository implements BookRepository {
+	
+	private Map<String, Book> books= new HashMap<>();
+	
+	
+	@Override
+	public Book save(Book book) {
+		books.put(book.getIsbn(), book);
+		return book;
+	}
+
+	@Override
+	public Optional<Book> findByIsbn(String isbn) {
+		return Optional.ofNullable(books.get(isbn));
+	}
+
+	@Override
+	public void deleteByIsbn(String isbn) {
+		if (books.containsKey(isbn)) books.remove(isbn);
+	}
+
+	@Override
+	public List<Book> findAll() {
+		List<Book> bookList= new ArrayList<>();
+		for (Book book: books.values()) {
+			bookList.add(book);
+		}
+		return bookList;
+	}
+
+}
