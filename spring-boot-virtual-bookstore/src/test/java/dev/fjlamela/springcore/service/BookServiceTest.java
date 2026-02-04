cbd1b6fde926aa9fd3612b423d16f1d4863663e7
Francisco José Lamela López <skonner87@gmail.com>
2026-02-04 19:07:19 +0100

    add unit test for duplicate isbn in BookService

--- spring-boot-virtual-bookstore/src/test/java/dev/fjlamela/springcore/service/BookServiceTest.java
+++ spring-boot-virtual-bookstore/src/test/java/dev/fjlamela/springcore/service/BookServiceTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import dev.fjlamela.springcore.domain.Book;
 import dev.fjlamela.springcore.repository.inmemory.InMemoryBookRepository;
+import dev.fjlamela.springcore.service.exception.DuplicateBookException;
 
 public class BookServiceTest {
 	
@@ -24,7 +25,15 @@ public class BookServiceTest {
 	
 	@Test
 	public void addBook_shouldThrowWhenDuplicateIsbn() {
+		InMemoryBookRepository memBook= new InMemoryBookRepository();
+		BookService bookService= new BookService(memBook);
+		Book book= new Book("The Old Man and the Sea", "Ernest Hemingway", "978-0684801223", 1952);
+		Book book2= new Book("The Old Man and the Sea", "Ernest Hemingway", "978-0684801223", 1952);
+		bookService.addBook(book);
 		
+		Assertions.assertThrows(DuplicateBookException.class, ()-> {			
+			bookService.addBook(book2);
+		});
 	}
 	
 	@Test
