950e4498bae073f9f6b8a0a9f5b360da708a6284
Francisco José Lamela López <skonner87@gmail.com>
2026-02-04 19:31:43 +0100

    add MockMvc test for listing books

---
+++ spring-boot-virtual-bookstore/src/test/java/dev/fjlamela/springcore/controller/BookControllerTest.java
@@ -0,0 +1,33 @@
+package dev.fjlamela.springcore.controller;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import dev.fjlamela.springcore.service.BookService;
+
+@WebMvcTest(BookController.class)
+public class BookControllerTest {
+	
+	@Autowired
+	private MockMvc mockMvc;
+	
+	@MockBean
+	private BookService bookService;
+	
+	
+	@Test
+	public void getBooks_shouldReturnOk() throws Exception {
+		when(bookService.listBooks()).thenReturn(List.of());
+		mockMvc.perform(get("/books")).andExpect(status().isOk());
+	}
+	
+}
